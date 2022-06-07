d8164f31b4a5b4188168fb2fa181049ec028db2d
gerlangiosoares <gerlangio@gmail.com>
2022-06-07 16:16:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert("[ERRO] Verifique os dados e tente novamente!");
     } else {
-        window.alert("Tudo OK");
+        /*window.alert("Tudo OK");*/
+        var fsex = window.document.getElementsByName("radsex");
+        var idade = ano - Number(fano.value);
+        /*res.innerHTML = `Idade calculada ${idade}`;*/
+        var genero = "";
+        if (fsex[0].checked) {
+            genero = "Homem";
+        } else if (fsex[1].checked) {
+            genero = "Mulher";
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
     }
 }
