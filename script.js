14d2e13ff76888481c3951f726604a723c931028
gerlangiosoares <gerlangio@gmail.com>
2022-06-07 15:35:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ function verificar() {
     /*window.alert("FUNCIONOU!");*/
     var data = new Date();
     var ano = data.getFullYear();
+    var fano = window.document.getElementById("txtano");
+    var res = window.document.querySelector("div#res");
+    /**
+     * pode ser assim também - || fano.value > ano
+     */
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!");
+    } else {
+        window.alert("Tudo OK");
+    }
 }
