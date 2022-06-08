2c107c2d0f9a42511e3a6d92e20a18e113821887
gerlangiosoares <gerlangio@gmail.com>
2022-06-08 09:42:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ function verificar() {
         var genero = "";
         if (fsex[0].checked) {
             genero = "Homem";
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+                window.alert("É uma criança!");
+            } else if (idade < 21) {
+                //JOVEM
+                window.alert("É um jovem!");
+            } else if (idade < 50) {
+                //ADULTO
+                window.alert("É um adulto!");
+            } else {
+                //IDOSO
+                window.alert("É um idoso!");
+            }
         } else if (fsex[1].checked) {
             genero = "Mulher";
         }
