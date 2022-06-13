89a61f13600b088a2b5f83aa042942feb3281bbc
gerlangiosoares <gerlangio@gmail.com>
2022-06-13 16:53:36 -0300

    Update formulas.js

--- formulas.js
+++ formulas.js
@@ -1,12 +1,26 @@
 /**
  * Repetições
  */
+/*
 var c = 1
 while(c <= 10) {
     //...
     c++
 }
 
-for () {
-    
-}
+for (var c = 1; c <= 10; c++) {
+
+}*/
+
+/*
+var c = 1;
+while (c <= 5) {
+    console.log(c);
+    c++;
+}*/
+
+var c = 1;
+do {
+    console.log(c);
+    c++;
+} while (c <= 5);
