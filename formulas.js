056e610bd9f4b975c06a8c19baf952ba796e3375
gerlangiosoares <gerlangio@gmail.com>
2022-06-20 13:19:28 -0300

    Update formulas.js

--- formulas.js
+++ formulas.js
@@ -25,8 +25,19 @@ do {
     c++;
 } while (c <= 5);
 */
+
+/*
 console.log('Vai começar...')
 for(var c = 1; c <= 4; c++){
     console.log(c)
 }
 console.log('FIM')
+*/
+
+//Arrays ou Vetores
+let num = [5,4,8];
+num[3]=6;
+num.push(7);
+num.length;
+num.sort();
+console.log(num);
